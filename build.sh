a2dbba1c12d16350eea0949ffe60894e5cae14b4
Ian Kirker <ian.kirker@gmail.com>
2019-02-19 16:28:00 +0000

    Makes build script build for macOS and Linux (amd64 and arm)

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@
 mkdir -p bin
 
 for dir in ./cmd/*; do
+  for arch_label in "" linux-amd64 linux-arm linux-arm64 darwin-amd64; do
+    export GOOS="${arch_label%%-*}"
+    export GOARCH="${arch_label##*-}"
     go build \
         -ldflags "-X main.buildDate=$(date +%Y-%m-%d) -X main.commitLabel=$(git rev-parse --short HEAD)" \
-        -o "bin/${dir##*/}" \
+        -o "bin/${dir##*/}${arch_label:+.}${arch_label}" \
         "$dir" 
+  done
 done
 
