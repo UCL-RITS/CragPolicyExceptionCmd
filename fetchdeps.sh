d1472ed3b753f1bbabf85d61c73cc05148617172
Ian Kirker <ian.kirker@gmail.com>
2019-02-20 20:18:17 +0000

    Adds survey package to deps (used for query build)

--- fetchdeps.sh
+++ fetchdeps.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # Set up build environment.
 
-if [ -z $GOPATH ]; then
+if [ -z "$GOPATH" ]; then
   echo "\$GOPATH is not set."
   exit 1
 else
   go get github.com/go-sql-driver/mysql
   go get github.com/olekukonko/tablewriter
   go get gopkg.in/alecthomas/kingpin.v2
+  go get gopkg.in/AlecAivazis/survey.v1
 fi
