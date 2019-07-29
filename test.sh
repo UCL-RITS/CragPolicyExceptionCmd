811f2c87378ef01993d46604c1dc819292fb0d64
Ian Kirker <ian.kirker@gmail.com>
2019-07-29 20:29:49 +0100

    Adds some basic tests

--- test.sh
+++ test.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
-# Placeholder
-true
+echo "travis_fold:start:Setting up config"
+
+TRAVIS_MYSQL_PORT="$(grep -e '^port=' "$HOME/.my.cnf" | sed -e 's/port=//')"
+
+# The location of the binary is somewhere in /tmp after running install.sh
+#  but we don't know where, since it's a mktemp directory.
+EXE="$(find /tmp/tmp.* -name "exceptions")"
+
+cat >"$HOME/.exceptions_db.conf" <<EOF
+{
+    "db_type": "mysql",
+    "db_connection_string": "travis:@tcp(127.0.0.1:$TRAVIS_MYSQL_PORT)/service_exceptions_test"
+}
+EOF
+
+echo "travis_fold:start:Setting up config"
+
+echo "travis_fold:start:Running tests"
+"$EXE" createdb
+"$EXE" destroydb
+"$EXE" createdb
+"$EXE" submit --user="someone" --service="myriad"
+"$EXE" submit --user="someone" --service="legion"
+"$EXE" submit --user="someone" --service="grace"
+"$EXE" submit --user="someone" --service="kathleen"
+"$EXE" submit --user="someone" --service="thomas"
+"$EXE" submit --user="someone" --service="michael"
+"$EXE" list
+echo "travis_fold:end:Running tests"
