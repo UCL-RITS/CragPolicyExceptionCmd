0b7bb360921ddf3fe6c494f2c8e66e8698f5ea11
Ian Kirker <ian.kirker@gmail.com>
2020-04-30 15:19:16 +0100

    Adding tests

--- test.sh
+++ test.sh
@@ -17,21 +17,47 @@ EOF
 
 echo "travis_fold:start:Setting up config"
 
+tmpdir="$(mktemp -d)"
+
 echo "travis_fold:start:Running tests"
 echo "Creating database schema..."
 "$EXE" createdb
+echo "Creating blank dump for comparison..."
+"$EXE" dumpjson >"$tmpdir/dump-blank.json"
 echo "Destroying database schema..."
 "$EXE" destroydb
 echo "Recreating database schema for rest of tests..."
 "$EXE" createdb
 echo "Submitting several entries..."
+# One for each cluster
 "$EXE" submit --username="someone" --service="myriad"
 "$EXE" submit --username="someone" --service="legion"
 "$EXE" submit --username="someone" --service="grace"
 "$EXE" submit --username="someone" --service="kathleen"
 "$EXE" submit --username="someone" --service="thomas"
 "$EXE" submit --username="someone" --service="michael"
+echo "Submitting an invalid entry (invalid clustername)..."
+if "$EXE" submit --username="someone" --service="XXXXXXX"; then
+  echo "Entry should have failed, instead succeeded."
+  false
+fi
 echo "Listing..."
 "$EXE" list
+echo "Testing dump and re-import..."
+"$EXE" dumpjson >"$tmpdir/dump-before.json"
+"$EXE" list >"$tmpdir/dump-before.list"
+echo "  Destroying and recreating db..."
+"$EXE" destroydb
+"$EXE" createdb
+echo "  Checking fresh blank dump matches old one..."
+"$EXE" dumpjson >"$tmpdir/dump-blank-2.json"
+diff -q "$tmpdir/dump-blank.json" "$tmpdir/dump-blank-2.json"
+echo "  Reimporting dump..."
+"$EXE" importjson <"$tmpdir/dump-before.json"
+"$EXE" list >"$tmpdir/dump-after.list"
+"$EXE" dumpjson >"$tmpdir/dump-after.json"
+echo "  Comparing before and after data..."
+diff -q "$tmpdir/dump-before.json" "$tmpdir/dump-after.json"
+diff -q "$tmpdir/dump-before.list" "$tmpdir/dump-after.list"
 echo "Complete."
 echo "travis_fold:end:Running tests"
