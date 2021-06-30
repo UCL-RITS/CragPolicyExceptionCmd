b5b89cd2e2db122e59a367bee947a9926a24a70f
Ian Kirker <ian.kirker@gmail.com>
2021-06-30 21:52:14 +0100

    Bumps install.sh Go version to 1.16.5

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ function check_for_go () {
         source /etc/profile.d/modules.sh
         module purge
         module load gcc-libs
-        module load compilers/go/1.12.4
+        module load compilers/go/1.16.5
     else
         echo "Could not get a go compiler, exiting..." >&2
         exit 1
